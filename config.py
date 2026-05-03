81f69f4cdad7149f3b3d1d59f88cbf4637847e2a
Kaif7607 <112199526+Kaif7607@users.noreply.github.com>
2026-05-03 13:40:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "20369373"))
+API_ID = int(os.environ.get("API_ID", "29426008"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "0d53cc7f978163fed3263be5cbb20ab0")
+API_HASH = os.environ.get("API_HASH", "fedd630ba4bd77044ee4e5a00e5300e6")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8464006684:AAHfz3jFQCaijVmG353odA9B90t-7-_Z2JE")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("Masterextratorraonebot")
+BOT_USERNAME = os.environ.get("J_geobot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5680454765"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1717411093"))
 # ------------------X------------------------------
-CREATOR_ID = int(os.environ.get("CREATOR_ID", "5680454765"))
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002585176866"))
+CREATOR_ID = int(os.environ.get("CREATOR_ID", "1717411093"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1003549344180"))
 
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5680454765").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1717411093").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002585176866"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003549344180"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://jilanirabbani1234_db_user:Jil%40ni7417@mondb.cxuaxsk.mongodb.net/?appName=Mondb")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002585176866"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003549344180"))
